ebe740be0f48eeec55840ee285be252bd1c49009
Antony J Malakkaran <antonyjm462@gmail.com>
2019-08-27 21:16:34 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MembersComponent } from './members/members.component';
+import { SpacesComponent } from './spaces/spaces.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    MembersComponent,
+    SpacesComponent,
+    ProfileComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
