4fc8e180087a808cb5e3148b0c7d0ac577b72e2c
ASWIN SURESH <aswinsureshp66@gmail.com>
2019-08-29 23:04:23 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MembersComponent } from './members/members.component';
+import { SpacesComponent } from './spaces/spaces.component';
+import { LogoutComponent } from './logout/logout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'spaces', component: SpacesComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { RegisterComponent } from './register/register.component';
+import { StopParentDirective } from './stop-parent.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RegisterComponent } from './register/register.component';
     ProfileComponent,
     LoginComponent,
     LogoutComponent,
-    RegisterComponent
+    RegisterComponent,
+    StopParentDirective
   ],
   imports: [
     BrowserModule,
