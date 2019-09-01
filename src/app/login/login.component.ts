1d6c655220bb1e064048614f501931565551c367
Antony J Malakkaran <antonyjm462@gmail.com>
2019-09-01 17:08:03 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { User } from './../model/user';
+import { DatabaseService } from '../database.service';
+import { Userlogin } from '../model/userlogin';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  UserForm: FormGroup;
+  User: Userlogin;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private database: DatabaseService, private router: Router) { }
 
   ngOnInit() {
+    this.UserForm = this.formBuilder.group({
+      Email: ['', Validators.required],
+      Password: ['', Validators.required],
+    });
   }
 
+  Login(form) {
+    this.User = form.value;
+    console.warn('Your user been submitted', this.User);
+    console.log(this.User);
+    this.database.login(form.value).subscribe((user: any) => {
+      console.log('User logined', user);
+      if (user) {
+        this.router.navigate(['dashboard']);
+      }
+    });
+  }
 }
